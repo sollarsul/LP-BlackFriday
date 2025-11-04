e431b886ad44783e2611982569aefd8ee1e46356
GabrielWadenphul <gabriel.wadenphul@universo.univates.br>
2025-11-04 17:54:00 +0000

    Remove specified sections and footer

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,11 @@
 import { Hero } from "@/components/Hero";
 import { BlackBox } from "@/components/BlackBox";
-import { Benefits } from "@/components/Benefits";
-import { Lives } from "@/components/Lives";
-import { Urgency } from "@/components/Urgency";
-import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
     <main className="min-h-screen bg-background">
       <Hero />
       <BlackBox />
-      <Benefits />
-      <Lives />
-      <Urgency />
-      <Footer />
     </main>
   );
 };
